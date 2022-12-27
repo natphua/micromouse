1afc0003126dc59b8b8bec977896413fa4d344ff
natphua <nataliephua10@gmail.com>
2022-12-27 11:19:30 -1000

    dfs islanded implementation

--- microMouseServer/studentai.cpp
+++ microMouseServer/studentai.cpp
@@ -1,8 +1,67 @@
 #include "micromouseserver.h"
+#include <stack>
+#include <vector>
 
 void microMouseServer::studentAI()
 {
-    static std::string movements{}; //variable that will track past movements
+    static std::string movements{};
+    static char last_turn{};
+    std::stack<char> stack;
+    while (stack)
+    {
+        // if there is a joint, add possible turns to stack
+        if (isWallForward() + isWallLeft() + isWallRight() == 1 or isWallForward() + isWallLeft() + isWallRight() == 0)
+        {
+            if (!isWallRight())
+            {
+                stack.push('R');
+            }
+            if (!isWallLeft())
+            {
+                stack.push('L');
+            }
+            if (!isWallForward()) // maybe delete this
+            {
+                stack.push('F');
+            }
+        }
+
+        // setting next path to take (right or left); forward is default
+        char next{stack.top};
+        switch (next)
+        {
+            case 'R':
+                turnRight();
+                last_turn = 'R';
+            case 'L':
+                turnLeft();
+                last_turn = 'L';
+        }
+        stack.pop();
+
+        moveForward();
+        movements += "F";
+
+        // unfinished
+        // add backtracking next and also finish condition
+
+    }
+}
+
+// issue: if I want to track visited, how will I record turns without x, y coords?
+// - try to backtrack using var movements?
+
+// depth first search algorithm using stacks (FILO)
+
+
+
+
+/*
+ * Unislanded solution:
+ *
+void microMouseServer::studentAI()
+{
+    static std::string movements{}; //tracks past movements
     moveForward();
     movements += "F";
     if (movements.size() > 6) //making sure index is not out of range
@@ -23,3 +82,4 @@ void microMouseServer::studentAI()
         movements += "L";
     }
 }
+*/
