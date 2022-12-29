5e9fb4b498393d1eea12d55be59845eae5a08665
natphua <nataliephua10@gmail.com>
2022-12-28 22:44:22 -1000

    reset; retrying with 2d arrays

--- microMouseServer/studentai.cpp
+++ microMouseServer/studentai.cpp
@@ -1,57 +1,84 @@
 #include "micromouseserver.h"
 #include <stack>
-#include <vector>
+#include <string>
+using namespace std;
+
+
+bool isCellValid(int maze[20][20], bool visited[20][20], int row, int col)
+{
+    // cell is out of bounds, already visited, or on a wall
+    if (row<0 || col<0 || row>20 || col>20 || visited[row][col] || maze[row][col] == '1')
+    {
+        return false;
+    }
+    return true;
+}
+
+void moveToCell(pair<int, int> current, pair<int, int> next)
+{
+    if (next.first > current.first)
+    {
+        moveForward(); // will fix function calls later
+    }
+    if (next.second > current.second)
+    {
+        turnRight();
+    }
+    else if (next.second < current.second)
+    {
+        turnLeft();
+    }
+}
+
 
 void microMouseServer::studentAI()
 {
-    static std::string movements{};
-    static char last_turn{};
-    std::stack<char> stack;
-    while (stack)
+    static string movements{}; // tracking past movements
+    static int maze[20][20], row, col;
+    static bool visited[20][20]; // 2d array matching maze to record whether visited or not
+    static stack <pair <int, int>> st;
+    st.push({0, 0}); // starting point
+
+    // setting all values in visited to false bc none have been visited yet
+    for (auto i=0; i<20; i++)
     {
-        // if there is a joint, add possible turns to stack
-        if (isWallForward() + isWallLeft() + isWallRight() == 1 or isWallForward() + isWallLeft() + isWallRight() == 0)
+        for (auto j=0; j<20; j++)
         {
-            if (!isWallRight())
-            {
-                stack.push('R');
-            }
-            if (!isWallLeft())
-            {
-                stack.push('L');
-            }
-            if (!isWallForward()) // maybe delete this
-            {
-                stack.push('F');
-            }
+            visited[i][j] = false;
+        }
+    }
+
+    while (st) // while stack is not empty
+    {
+        pair<int, int> current = st.top;
+        st.pop();
+        row = current.first;
+        col = current.second;
+
+        // marking surroundings and saving to maze record
+        if (isWallForward())
+        {
+            // ISSUE: orientation of mouse can confuse location of walls using +1 -1 for right/left/forward
         }
 
-        // setting next path to take (right or left); forward is default
-        char next{stack.top};
-        switch (next)
+
+        if (isCellValid(maze, visited, row, col))
         {
-            case 'R':
-                turnRight();
-                last_turn = 'R';
-            case 'L':
-                turnLeft();
-                last_turn = 'L';
+            continue;
         }
-        stack.pop();
 
-        moveForward();
-        movements += "F";
+        moveToCell(current, next); // need to sort out diff between current and next with stack
+        visited[row][col] = true;
 
-        // unfinished
-        // add backtracking next and also finish condition
+        st.push({row+1, col}); // go forward
+        st.push({row, col-1}); // go left
+        st.push({row, col+1}); // go right
+
+        // unfinished; need to add backtracking, fix maze array, and sort out functions
 
     }
 }
 
-// issue: if I want to track visited, how will I record turns without x, y coords?
-// - try to backtrack using var movements?
-
-// depth first search algorithm using stacks (FILO)
 
 
 
